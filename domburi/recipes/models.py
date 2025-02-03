3bc7f7b7f8d633d204b9ca86ac4949ff89788360
Grishaak <antonianogroboracevskij@gmail.com>
2025-02-04 01:16:41 +0300

    Added new entites for porject. New views.

--- domburi/recipes/models.py
+++ domburi/recipes/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import ForeignKey, CharField, TextField, ImageField, IntegerField
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = CharField(max_length=40, null=False)
+    description = TextField(max_length=2000, null=False, blank=True)
+    cooking_steps = IntegerField(default=1)
+    cooking_time = IntegerField(default=5)
+    image = ImageField(null=True, blank=True)
+    author = ForeignKey(User, on_delete=models.CASCADE, related_name='recipe')
+
+    def __str__(self):
+        return f'Рецепт: {self.name}.'
--- domburi/recipes/views.py
+++ domburi/recipes/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from recipes.models import Recipe
+
 
 def index(request):
-    return HttpResponse("<h2>Start django recipes site.</h2>")
+    return render(request, template_name='recipes/index.html')
+
+
+def get_recipes(request):
+    context = {
+        'recipes':
+            Recipe.objects.all()
+    }
+    return render(request, template_name='recipes/recipes.html', context=context)
