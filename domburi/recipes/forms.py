3c6747e365a8a34884b2570c070a20d03520bd97
Grishaak <antonianogroboracevskij@gmail.com>
2025-02-09 21:32:38 +0300

    Added new viewpoints and templates.

---
+++ domburi/recipes/forms.py
@@ -0,0 +1,40 @@
+from django.core.exceptions import ValidationError
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.db.models import TextField
+from django.forms import forms, CharField, EmailField, IntegerField, Textarea, FileField, MultipleChoiceField, \
+    CheckboxSelectMultiple, ImageField, ModelForm
+from .models import Recipe
+
+from recipes.models import Category
+
+
+class UserForm(forms.Form):
+    name = CharField(max_length=50, min_length=3, label='Name')
+    email = EmailField(label='Email')
+    firstname = CharField(max_length=50, min_length=1, label='Firstname')
+    surname = CharField(max_length=50, min_length=1, label='Surname')
+
+
+def validate_file(file: InMemoryUploadedFile):
+    print(file.file, file.content_type)
+    validated_img_types = ('image/jpeg', 'image/png', 'image/gif', 'image/jpg', 'image/webp')
+    if file.content_type not in validated_img_types:
+        raise ValidationError('File is not in right type. Should be jpeg, png, gif.')
+    elif file.size > 1000000:
+        raise ValidationError('File should be less than 1mb size.')
+
+
+# class RecipeForm(forms.Form):
+#     name = CharField(max_length=40, label='Name')
+#     description = CharField(widget=Textarea, label='Description')
+#     cooking_steps = IntegerField(min_value=1, max_value=10, label='Cooking Steps')
+#     cooking_time = IntegerField(min_value=5, max_value=1440, label='Cooking Time')
+#     image = ImageField(required=False, validators=[validate_file, ])
+#     categories = MultipleChoiceField(choices=((i.pk, i.name) for i in Category.objects.all()),
+#                                      widget=CheckboxSelectMultiple)
+
+
+class RecipeForm(ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['name', 'description', 'cooking_steps', 'cooking_time', 'image', 'categories']
--- domburi/recipes/views.py
+++ domburi/recipes/views.py
@@ -1,11 +1,30 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, redirect
+import os
 
+from django.urls import reverse
+from django.views import View
+from django.views.generic import ListView, DetailView
+
+from recipes.forms import UserForm, RecipeForm
 from recipes.models import Recipe
 
 
 def index(request):
     return render(request, template_name='recipes/index.html')
+
+
+class RecipeGeneralView(ListView):
+    queryset = Recipe.objects.select_related('author').prefetch_related('categories').all()
+    context_object_name = 'recipes'
+    template_name = 'recipes/recipes.html'
+
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    context_object_name = 'recipe'
+    template_name = 'recipes/detailed_recipe.html'
 
 
 def get_recipes(request):
@@ -14,3 +33,54 @@ def get_recipes(request):
             Recipe.objects.select_related('author').prefetch_related('categories').all()
     }
     return render(request, template_name='recipes/recipes.html', context=context)
+
+
+def upload_file(request):
+    if request.method == 'POST' and request.FILES.get('myfile'):
+        print('In post')
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        print(filename, "saved")
+        context = {
+            'data': 'File is uploaded just fine.'
+        }
+    else:
+        print('No content and method is not POST')
+        context = {}
+    return render(request, template_name='recipes/upload_file.html', context=context)
+
+
+def user_form(request):
+    context = {
+        'form': UserForm(),
+    }
+    return render(request, template_name='recipes/user_form.html', context=context)
+
+
+def user_validate(request: HttpRequest):
+    if request.user.pk is not None:
+        return True
+    return False
+
+
+def create_recipe(request: HttpRequest):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid() and user_validate(request):
+            data = form.cleaned_data
+            cats = form.cleaned_data.pop('categories')
+            data['author_id'] = request.user.pk
+            instance = Recipe.objects.create(**data)
+            instance.categories.set(cats)
+            url_revers = reverse('recipes:recipes_list')
+            return redirect(url_revers)
+        # else:
+        #     url_revers = reverse('recipes:create_recipe')
+        #     return redirect(url_revers)
+    else:
+        form = RecipeForm()
+    context = {
+        'form': form
+    }
+    return render(request, template_name='recipes/create_recipe.html', context=context)
